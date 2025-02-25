b957450d9906837b0bfcc9195944fe231ec98791
cypriankanda <cyprianboit@gmail.com>
2025-02-26 01:29:13 +0300

    write card commit

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+// Function to create thank you cards
+function writeCards(names, event) {
+    let messages = [];
+    for (let i = 0; i < names.length; i++) {
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return messages;
+}
+
+// Function to count down from a given number to 0
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number);
+        number--;
+    }
+}
+
+// Example usage
+console.log(writeCards(["Charlie", "Samip", "Ali"], "birthday"));
+countDown(10);
